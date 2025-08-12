5a2dcbf9e0ac279007c3efff08b5518197511df8
nahom <66465737+nahoomg@users.noreply.github.com>
2025-08-12 21:33:17 +0000

    Add USE statement to task_3.sql for clarity on database context

--- task_3.sql
+++ task_3.sql
@@ -3,4 +3,6 @@
 -- List all tables in the currently selected database.
 -- The database name (alx_book_store) will be passed as an argument
 -- to the mysql command when executing this script.
+
+USE alx_book_store;
 SHOW TABLES;
